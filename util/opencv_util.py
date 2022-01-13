a5475d32b43aa8ae849a185dd42e99af1f1f1d0b
quicksandzn <quicksandzn@gmail.com>
2022-01-13 15:22:51 +0800

    init fake

---
+++ util/opencv_util.py
@@ -0,0 +1,72 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import requests
+from PIL import Image
+from io import BytesIO
+
+
+def opencv_match_template_coord(bg_path, slide_block_path, tmp_path):
+    """
+        opencv 模板匹配 计算二维位置
+    :param bg_path:  背景图
+    :param slide_block_path: 滑动块
+    :param tmp_path: tmp
+    :return: x,y
+    """
+    # 读取图片
+    bg = cv2.imread(bg_path)
+    slide_block = cv2.imread(slide_block_path)
+    # 灰度处理
+    bg = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
+    slide_block = cv2.cvtColor(slide_block, cv2.COLOR_BGR2GRAY)
+    slide_block = abs(255 - slide_block)
+    cv2.imwrite(tmp_path, slide_block)
+    # 去掉滑块黑色部分 # 0表示黑色，1表示高亮部分
+    slide_block = slide_block[slide_block.any(1)]
+    # 匹配->cv图像匹配算法 match匹配,Template模板;精度高，速度慢的方法
+    result = cv2.matchTemplate(bg, slide_block, cv2.TM_CCOEFF_NORMED)
+    # 返回的是一维的位置，最大值索引
+    index_max = np.argmax(result)
+    # 反着推最大值的二维位置，和opencv是相反的
+    x, y = np.unravel_index(index_max, result.shape)
+
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    print("----------------------------")
+    print("min_val", min_val)
+    print("max_val", max_val)
+    print("min_loc", min_loc)
+    print("max_loc", max_loc)
+    print("----------------------------")
+    h, w = slide_block.shape[:2]
+    bottom_right = (max_loc[0] + w, max_loc[1] + h)
+    # cv2.circle(result, max_loc, 10, 0, 2)
+    # 画矩形|
+    cv2.rectangle(bg, max_loc, bottom_right, (0, 255, 0), 2)
+    plt.subplot(121), plt.imshow(result, cmap='gray')
+    plt.subplot(122), plt.imshow(bg, cmap='gray')
+    plt.show()
+
+    return x, y
+
+
+def download_image(filepath, img_url):
+    """
+        download image
+    :param filepath: save path
+    :param img_url: img_url
+    :return: image
+    """
+    response = requests.get(url=img_url)
+    new_im: Image = Image.open(BytesIO(response.content))
+    new_im.convert('RGB')
+    new_im.save(filepath)
+    return new_im
+
+
+if __name__ == '__main__':
+    bg_path = '../file/bg.png'
+    slide_block_path = '../file/block.png'
+    tmp_path = '../file/tmp.png'
+    x, y = opencv_match_template_coord(bg_path=bg_path, slide_block_path=slide_block_path, tmp_path=tmp_path)
+    print(x, y)
---
+++ util/selenium_util.py
@@ -0,0 +1,91 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+import os
+import re
+import zipfile
+
+class ChromeDriver(object):
+    """
+        chrome driver
+    """
+
+    def __init__(self, privacy_proxy=None, proxy=None,
+                 headless=False, driver_name='mac_chromedriver_96.0.4664.45'):
+
+        self.up_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        self.driver_path = os.path.join(self.up_path, 'chrome_driver', driver_name)
+        self.options = Options()
+        # common default settings
+        self.options.add_argument('--dns-prefetch-disable')
+        self.options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+        self.options.add_argument('disable-infobars')  # 隐藏'Chrome正在受到自动软件的控制'
+        self.options.add_argument('--no-sandbox')
+        self.options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        if headless:
+            self.options.add_argument('--headless')  # 增加无界面选项
+        # 私密代理
+        if privacy_proxy:
+            self.options.add_extension(self.get_chrome_proxy_extension(proxy=privacy_proxy))
+        # 普通代理
+        if proxy:
+            self.options.add_argument('--proxy-server=%s' % proxy)
+
+        self.driver = webdriver.Chrome(executable_path=self.driver_path, options=self.options)
+
+    def get_chrome_proxy_extension(self, proxy):
+        """
+                获取一个Chrome代理扩展,里面配置有指定的代理(带用户名密码认证)
+                proxy - 指定的代理,格式: username:password@ip:port
+        """
+
+        # Chrome代理模板插件(https://github.com/RobinDev/Selenium-Chrome-HTTP-Private-Proxy)
+        chrome_proxy_helper_dir = os.path.join(self.up_path, 'chrome-proxy-helper', '')
+        custom_chrome_proxy_extensions_dir = os.path.join(self.up_path, 'chrome-proxy-extensions', '')
+        m = re.compile('([^:]+):([^\@]+)\@([\d\.]+):(\d+)').search(proxy)
+        if m:
+            # 提取代理的各项参数
+            username = m.groups()[0]
+            password = m.groups()[1]
+            ip = m.groups()[2]
+            port = m.groups()[3]
+            # 创建一个定制Chrome代理扩展(zip文件)
+            if not os.path.exists(custom_chrome_proxy_extensions_dir):
+                os.mkdir(custom_chrome_proxy_extensions_dir)
+            extension_file_path = os.path.join(custom_chrome_proxy_extensions_dir,
+                                               '{}.zip'.format(proxy.replace(':', '_')))
+            if not os.path.exists(extension_file_path):
+                # 扩展文件不存在，创建
+                zf = zipfile.ZipFile(extension_file_path, mode='w')
+                zf.write(os.path.join(chrome_proxy_helper_dir, 'manifest.json'), 'manifest.json')
+                # 替换模板中的代理参数
+                background_content = open(os.path.join(chrome_proxy_helper_dir, 'background.js')).read()
+                background_content = background_content.replace('%proxy_host', ip)
+                background_content = background_content.replace('%proxy_port', port)
+                background_content = background_content.replace('%username', username)
+                background_content = background_content.replace('%password', password)
+                zf.writestr('background.js', background_content)
+                zf.close()
+            return extension_file_path
+        else:
+            raise Exception('Invalid proxy format. Should be username:password@ip:port')
+
+    def get_cookies(self):
+        """
+            get cookies
+        :return:
+        """
+        self.driver.refresh()
+        cookies = self.driver.get_cookies()
+        return cookies
+
+    def trace_and_move(self, slider, x, y, ):
+        """
+            trace_and_move
+        :param slider: 一般情况下是滑动按钮区域
+        :param x: x
+        :param y: y
+        """
+
+        ActionChains(self.driver).drag_and_drop_by_offset(slider, xoffset=(y - 56) / 2, yoffset=0).perform()
+        ActionChains(self.driver).release(slider).perform()
